8270849d24efb719ac217cc91e7551289e20e7c1
= <saculbertazo@gmail.com>
2023-06-09 19:57:47 -0300

    Segunda modificação

--- src/macaquinhos/Macaquinhos.java
+++ src/macaquinhos/Macaquinhos.java
@@ -4,6 +4,7 @@ public class Macaquinhos {
 
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Outra modificação!");
     }
     
 }
