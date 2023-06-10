6060e42a71df1f2ab7c4e6725e73692900367892
= <saculbertazo@gmail.com>
2023-06-10 00:31:53 -0300

    testes roubo

--- src/macaquinhos/Macaquinhos.java
+++ src/macaquinhos/Macaquinhos.java
@@ -5,5 +5,8 @@ public class Macaquinhos {
     public static void main(String[] args) {
         Macaco macaco = new Macaco("jose", 10, 25, 15, 30, 5);
         Macaco m2 = new Macaco("jose", 40, 20, 5, 50, 15);
+        m2.Distrair(10);
+        macaco.Roubar(m2);
+        macaco.Roubar(m2);
     }
 }
