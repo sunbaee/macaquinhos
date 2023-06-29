553bae62390045a80acf235b23f00b03733a2cbc
Lucas Bertazo de Deus Felix - 1302313641 <1302313641@esade.local>
2023-06-29 11:13:31 -0300

    buff macaco custom

--- src/macaquinhos/Macacos/Custom.java
+++ src/macaquinhos/Macacos/Custom.java
@@ -8,4 +8,25 @@ public class Custom extends Macaco {
         this.ambiente = Ambiente.ambientes[0];
         this.tipo = 0;
     }
+    
+    @Override
+    public int coletar() {
+        int numPedras = Macaco.random(1, taxaColeta);
+        this.pedras += numPedras;
+        this.taxaDefesa += 1;
+
+        return numPedras;
+    }
+    
+    @Override
+    public void mostrarDados() {
+        System.out.println("\n   " + this.nome + "\n" +
+                           "\n > PEDRAS INICIAIS: " + this.pedras + 
+                           "\n > TAXA DE COLETA: " + this.taxaColeta +
+                           "\n > CHANCE DE ROUBO: " + this.taxaRoubo + 
+                           "\n > PEDRAS DO ROUBO: " + this.qntMaxRoubo / 2 + " ~ " + this.qntMaxRoubo +
+                           "\n > DEFESA INICIAL: " + this.defesaInicial +
+                           "\n > AMBIENTE: " + this.ambiente.getNome() + 
+                           "\n > PASSIVA DE CUSTOM: Ao coletar, ganha +1 DEF até tentarem roubar-lhe.");
+    }
 }
